867686f02d85a626213d4a4f72b6bc9227b17b6d
Kelsie Dustin <kellodust@gmail.com>
2019-10-08 23:45:43 -0600

    Added folder

---
+++ portfolio/scripts/modularize.js
@@ -0,0 +1,14 @@
+w3.includeHTML(
+    
+    function()
+    {
+    // call function to get date
+
+
+    // call function to set active link
+    setActiveLink();
+        
+  //  services();
+
+}
+);
