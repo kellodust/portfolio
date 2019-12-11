d41b1354732ec98669c55022378f6c2ffb8272bf
Kelsie Dustin <kellodust@gmail.com>
2019-12-11 00:04:03 -0700

    no more button

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,5 +1,5 @@
 function toggleNav() {
 	var updateElement = document.getElementById("menu-icon");
- //toggle adds a class if it's not there or removes it if it  itis.
+ //toggle adds a class if it's not there or removes it if it is.
  updateElement.classList.toggle("open");
 } 
